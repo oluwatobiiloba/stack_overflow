5ea17dba03dcd15f6aab83bbaa116c7a96dd548d
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-02-21 12:24:41 +0100

    pm2 init

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps : [{
+    script: 'app.js',
+    watch: '.',
+    instances: 0,
+    exec_mode: "cluster"
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
