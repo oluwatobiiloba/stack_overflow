9fe575aa7a3c1ddb4915343cbcf49a19ccb311fb
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-03-02 10:55:33 +0100

    FIX TEST REDIS ISSUE

--- config/config.js
+++ config/config.js
@@ -57,7 +57,9 @@ module.exports = {
     DD_SITE: process.env.DD_SITE,
     NODE_OPTIONS: process.env.NODE_OPTIONS,
     UV_THREADPOOL_SIZE: OS.cpus().length,
-    WORKER_POOL_ENABLED: process.env.WORKER_POOL_ENABLED || true
+    WORKER_POOL_ENABLED: process.env.WORKER_POOL_ENABLED || true,
+    AZURE_CACHE_FOR_REDIS_HOST_NAME: process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME,
+    AZURE_CACHE_FOR_REDIS_ACCESS_KEY: process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY
   },
   test_github_actions: {
     username: 'root',
@@ -82,7 +84,9 @@ module.exports = {
     DD_API_KEY: process.env.DD_API_KEY,
     DD_SITE: process.env.DD_SITE,
     UV_THREADPOOL_SIZE: OS.cpus().length,
-    WORKER_POOL_ENABLED: process.env.WORKER_POOL_ENABLED || true
+    WORKER_POOL_ENABLED: process.env.WORKER_POOL_ENABLED || true,
+    AZURE_CACHE_FOR_REDIS_HOST_NAME: process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME,
+    AZURE_CACHE_FOR_REDIS_ACCESS_KEY: process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY
   },
 
   production: {
@@ -110,7 +114,9 @@ module.exports = {
     DD_SITE: process.env.DD_SITE,
     NODE_OPTIONS: process.env.NODE_OPTIONS,
     UV_THREADPOOL_SIZE: OS.cpus().length - 2,
-    WORKER_POOL_ENABLED: process.env.WORKER_POOL_ENABLED || true
+    WORKER_POOL_ENABLED: process.env.WORKER_POOL_ENABLED || true,
+    AZURE_CACHE_FOR_REDIS_HOST_NAME: process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME,
+    AZURE_CACHE_FOR_REDIS_ACCESS_KEY: process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY
   },
 };
 
--- util/redis_helper.js
+++ util/redis_helper.js
@@ -1,6 +1,13 @@
 const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config.js`)[env];
-module.exports = require('redis').createClient({
-    url: `rediss://${config.AZURE_CACHE_FOR_REDIS_HOST_NAME}:6380`,
-    password: config.AZURE_CACHE_FOR_REDIS_ACCESS_KEY + '=',
-});
+let connection_options = null
+if (config.NODE_ENV === 'development') {
+    connection_options = {
+        url: `rediss://${config.AZURE_CACHE_FOR_REDIS_HOST_NAME}:6380`,
+        password: config.AZURE_CACHE_FOR_REDIS_ACCESS_KEY + '=',
+    }
+} else {
+    connection_options = process.env.REDIS_URL
+}
+console.log('connection_options', connection_options)
+module.exports = require('redis').createClient(connection_options);
