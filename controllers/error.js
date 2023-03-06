45b3a5313caf6af89eeb25d139dcf9ea8223258e
Oluwatobiloba Aremu <oluwatobiloba.f.a@gmail.com>
2023-03-06 01:24:31 +0100

    fix deep source

--- controllers/error.js
+++ controllers/error.js
@@ -71,7 +71,7 @@ const sendErrorDev = (req, err, res) => {
  * @param {object} res - response object
  * @returns {object} - JSON object with status code, status, and message
  */
-const sendErrorProd = (_req, err, res) => {
+const sendErrorProd = (err, res) => {
     const { statusCode = 500, status = "error", message } = err;
 
     if (err.isOperational) {
@@ -110,6 +110,6 @@ module.exports = (err, req, res) => {
         if (error.name === 'CastError') error = castError(error);
         if (error.name === "SequelizeUniqueConstraintError") error = duplicateFieldsDB(error);
         if (error.name === 'ValidationError') error = validationErrorDB(error);
-        sendErrorProd(req, error, res);
+        sendErrorProd(error, res);
     }
 };
